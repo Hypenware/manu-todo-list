5da1932419d784606eeb0012c974c17d9cce9b63
Manu Sharma <pritish.sharma@tweedl.in>
2023-03-20 13:53:51 +0530

    list through for loop

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  todos: Todo[];
+  constructor() {
+    this.todos=[
+      {
+        sno: 1,
+        title: "obj1",
+        desc: "this is decription",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "obj2",
+        desc: "this is decription",
+        active: true,
+      },
+      {
+        sno: 3,
+        title: "obj3",
+        desc: "this is decription",
+        active: true
+      }
+    ]
+  }
+  ngOnInit(): void {
+    
+  }
 
 }
